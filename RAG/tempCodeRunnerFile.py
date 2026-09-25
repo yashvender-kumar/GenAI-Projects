2af7b977dec6109214b25f901b4443011d4c9771
Yash <aadiemail.com>
2026-09-25 10:19:07 +0530

    Updeated gitignore

--- RAG/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-while True:
-    query = input("User: ")
-
-    if query.lower() in ["exit", "quit", "bye"]:
-        print("Good Bye Have A Nice Day!")
-        break
-
-res = rag_chain.invoke(query)
-print("AI:", res, "\n")  
--- apps/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-streamlit run apps/1_QNA.py 
